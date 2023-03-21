73ff664418866c527aebc8d5fe17cecfa22a12e8
keelybroderick <98989354+keelybroderick@users.noreply.github.com>
2023-03-20 23:28:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ async function getBooks() {
     .select('*')
 
   for (let book of books) {
-    let bookList = document.getElementById('books');
-    bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td></tr>`;
-  }
+  let bookList = document.getElementById('books');
+  bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td><td>${book.description}</td></tr>`;}
 
 getBooks();
